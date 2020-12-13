7db0554e51fb6218d6b8d309aa5c750409512fef
kai.yamada <yama0630abroad@yahoo.co.jp>
2020-12-13 08:41:45 +0000

    for heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
-  config.hosts.clear
+  config.hosts << "radio-introduction.herokuapp.com"
 end

caf827c3f6d49e057c599ff0fcd688a5771f7d6b
kai.yamada <yama0630abroad@yahoo.co.jp>
2020-12-18 00:37:26 +0000

    for heroku

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,6 @@ module Radios
     config.hosts << 'abc570c2f4554c269935522a6fcbedd5.vfs.cloud9.us-east-1.amazonaws.com'
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.hosts << "favorite-radios.herokuapp.com/"
   end
 end
